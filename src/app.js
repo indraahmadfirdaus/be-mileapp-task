8986e51e49897ac7e086713b83a75680ee408b47
Indra <indraahmadfirdaus@gmail.com>
2025-09-30 20:17:59 +0700

    feat: init project

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.json({
+    success: true,
+    message: 'Welcome to MileApp API',
+    version: '1.0.0'
+  });
+});
+
+module.exports = app;
